1bac7b551c50899f8d17a36f22c34625ea1ae29e
moyuboo1 <moyuboo1@keio.jp>
2023-09-05 15:33:40 +0900

    FIX: sh file path

--- web.sh
+++ web.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
-cd ~/ros2_ws/src/hi_gui/hi_gui_app && npm run dev
+cd ~/ros2_ws/src/catch2023/hi_gui/hi_gui_app && npm run dev
